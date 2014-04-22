73cfb36ec07b97cb23776de72fc302286b328f27
Heiko Heiko <heiko@heiko.org>
2014-04-22 16:37:41 +0200

    fix: initialize with signature; support deserialization of encoded data

--- pyethereum/transactions.py
+++ pyethereum/transactions.py
@@ -2,8 +2,9 @@ import re
 import rlp
 from bitcoin import encode_pubkey
 from bitcoin import ecdsa_raw_sign, ecdsa_raw_recover
-from utils import big_endian_to_int as decode_int
-from utils import int_to_big_endian as encode_int
+from opcodes import reverse_opcodes
+from utils import big_endian_to_int
+from utils import int_to_big_endian
 from utils import sha3, privtoaddr
 import utils
 import sys
@@ -28,58 +29,28 @@ class Transaction(object):
         0 <= r < P - N if v >= 2), and
     (ii) the sending account has enough funds to pay the fee and the value.
     """
-
     # nonce,value,gasprice,startgas,to,data
-    def __init__(*args):
-        self = args[0]
-        if len(args) == 2:
-            self.parse(args[1])
-        else:
-            self.nonce = args[1]
-            self.value = args[2]
-            self.gasprice = args[3]
-            self.startgas = args[4]
-            self.to = utils.coerce_addr_to_bin(args[5])
-            self.data = args[6]
-            # includes signature
-            if len(args) > 7:
-                self.v, self.r, self.s = args[7:10]
-                if self.r > 0 and self.s > 0:
-                    rawhash = sha3(rlp.encode(self.serialize(False)))
-                    pub = encode_pubkey(
-                        ecdsa_raw_recover(rawhash, (self.v, self.r, self.s)),
-                        'bin')
-                    self.sender = sha3(pub[1:])[-20:].encode('hex')
-            # does not include signature
-            else:
-                self.v, self.r, self.s = 0, 0, 0
-                self.sender = 0
 
-    # nonce,value,gasprice,startgas,code
-    @classmethod
-    def contract(*args):
-        sys.stderr.write(
-            "Deprecated method. Use pyethereum.transactions.contract " +
-            "instead of pyethereum.transactions.Transaction.contract\n")
-        return contract(*args[1:])
 
-    @classmethod
-    def parse(cls, data):
-        if re.match('^[0-9a-fA-F]*$', data):
-            data = data.decode('hex')
-        o = rlp.decode(data) + ['', '', '']
-        tx = cls(decode_int(o[0]),
-                 decode_int(o[1]),
-                 decode_int(o[2]),
-                 decode_int(o[3]),
-                 o[4].encode('hex'),
-                 o[5],
-                 decode_int(o[6]),
-                 decode_int(o[7]),
-                 decode_int(o[8]))
-        return tx
+    def __init__(self, nonce, value, gasprice, startgas, to, data, v=0, r=0, s=0):
+        self.nonce = int(nonce)
+        self.value = int(value)
+        self.gasprice = int(gasprice)
+        self.startgas = int(startgas)
+        self.to = utils.coerce_addr_to_bin(to)
+        self.data = data
+        self.v, self.r, self.s = int(v), int(r), int(s)
 
-    def sign(self, key):
+        self.sender = 0
+
+        # includes signature
+        if self.r > 0 and self.s > 0:
+            rawhash = self.serialize(False)
+            pub = encode_pubkey(
+                ecdsa_raw_recover(rawhash, (self.v, self.r, self.s)), 'bin')
+            self.sender = sha3(pub[1:])[-20:].encode('hex')
+
+   def sign(self, key):
         rawhash = sha3(rlp.encode(self.serialize(False)))
         self.v, self.r, self.s = ecdsa_raw_sign(rawhash, key)
         self.sender = privtoaddr(key)
@@ -89,24 +60,43 @@ class Transaction(object):
         return n.encode('hex') if len(n) == 20 else n
 
     def serialize(self, signed=True):
-        return rlp.encode([encode_int(self.nonce),
-                           encode_int(self.value),
-                           encode_int(self.gasprice),
-                           encode_int(self.startgas),
+        return rlp.encode([int_to_big_endian(self.nonce),
+                           int_to_big_endian(self.value),
+                           int_to_big_endian(self.gasprice),
+                           int_to_big_endian(self.startgas),
                            utils.coerce_addr_to_bin(self.to),
                            self.data,
-                           encode_int(self.v),
-                           encode_int(self.r),
-                           encode_int(self.s)][:9 if signed else 6])
+                           int_to_big_endian(self.v),
+                           int_to_big_endian(self.r),
+                           int_to_big_endian(self.s)][:9 if signed else 6])
 
     def hex_serialize(self):
         return self.serialize().encode('hex')
 
     def hash(self):
         return sha3(self.serialize())
+    # nonce,value,gasprice,startgas,code
+    
+
+    @classmethod
+    def contract(*args):
+        sys.stderr.write(
+            "Deprecated method. Use pyethereum.transactions.contract " +
+            "instead of pyethereum.transactions.Transaction.contract\n")
+        return contract(*args[1:])
 
 
 def contract(nonce, endowment, gasprice, startgas, code, v=0, r=0, s=0):
     tx = Transaction(nonce, endowment, gasprice, startgas, '', code)
     tx.v, tx.r, tx.s = v, r, s
     return tx
+
+
+def cast_transaction_args_from_rlp_decoded(nonce, value, gasprice, startgas, to, data, v=0, r=0, s=0):
+    "rlp encoding is not aware of types"
+    d = big_endian_to_int
+    return [d(nonce), d(value), d(gasprice), d(startgas), to.encode('hex'), data, d(v), d(r), d(s)]
+
+
+def transaction_from_rlp_encoded(data):
+    return Transaction(cast_transaction_args_from_rlp_decoded(rlp.decode(data)))
